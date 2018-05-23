7ee7e5e9bcb3d21ec4341da3debf168a24668783
yuchao <305694029@qq.com>
2018-05-24 00:38:18 +0800

    更新

--- project/skins/wj_cn/index/list/list_0.php
+++ project/skins/wj_cn/index/list/list_0.php
@@ -3,7 +3,7 @@
 ?>
 <div class="idx_news">
     <div class="idx_news_list">
-        <dl><a href="###" title="<?php echo sys_href($data['channelId'],'list',$data['id'])?>">
+        <dl><a href="###" title="<?php echo $data['sourceUrl']?>">
                 <dt><img src="<?php echo ispic($data['originalPic']); ?>" width="100%" /></dt>
                 <dd class="title"><?php echo $data['title']; ?></dd>
                 <dd class="details">
